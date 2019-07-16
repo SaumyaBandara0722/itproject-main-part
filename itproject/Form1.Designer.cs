14c62fd4d7cf2d93c928bc4a8dad8e3eadb8ff76
Amith Mihiranga <lwpamihiranga@gmail.com>
2019-07-16 16:54:06 +0530

    interface update

--- itproject/Form1.Designer.cs
+++ itproject/Form1.Designer.cs
@@ -28,17 +28,39 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.sidePanel = new System.Windows.Forms.Panel();
             this.topPanel = new System.Windows.Forms.Panel();
+            this.logoPanel = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonEmployees = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.buttonLeaveRequestPortal = new System.Windows.Forms.Button();
+            this.buttonLeaveManagement = new System.Windows.Forms.Button();
+            this.buttonPayroll = new System.Windows.Forms.Button();
+            this.buttonCustomers = new System.Windows.Forms.Button();
+            this.buttonProjectsAndTasks = new System.Windows.Forms.Button();
+            this.buttonStocks = new System.Windows.Forms.Button();
+            this.sidePanel.SuspendLayout();
+            this.logoPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // sidePanel
             // 
             this.sidePanel.BackColor = System.Drawing.Color.DimGray;
+            this.sidePanel.Controls.Add(this.buttonStocks);
+            this.sidePanel.Controls.Add(this.buttonProjectsAndTasks);
+            this.sidePanel.Controls.Add(this.buttonCustomers);
+            this.sidePanel.Controls.Add(this.buttonPayroll);
+            this.sidePanel.Controls.Add(this.buttonLeaveManagement);
+            this.sidePanel.Controls.Add(this.buttonLeaveRequestPortal);
+            this.sidePanel.Controls.Add(this.button2);
+            this.sidePanel.Controls.Add(this.buttonEmployees);
             this.sidePanel.Dock = System.Windows.Forms.DockStyle.Left;
             this.sidePanel.Location = new System.Drawing.Point(0, 0);
             this.sidePanel.Name = "sidePanel";
-            this.sidePanel.Size = new System.Drawing.Size(294, 715);
+            this.sidePanel.Size = new System.Drawing.Size(294, 815);
             this.sidePanel.TabIndex = 0;
             // 
             // topPanel
@@ -47,18 +69,125 @@
             this.topPanel.Dock = System.Windows.Forms.DockStyle.Top;
             this.topPanel.Location = new System.Drawing.Point(294, 0);
             this.topPanel.Name = "topPanel";
-            this.topPanel.Size = new System.Drawing.Size(755, 10);
+            this.topPanel.Size = new System.Drawing.Size(941, 10);
             this.topPanel.TabIndex = 1;
+            // 
+            // logoPanel
+            // 
+            this.logoPanel.BackColor = System.Drawing.Color.LightBlue;
+            this.logoPanel.Controls.Add(this.pictureBox1);
+            this.logoPanel.Location = new System.Drawing.Point(351, 0);
+            this.logoPanel.Name = "logoPanel";
+            this.logoPanel.Size = new System.Drawing.Size(165, 217);
+            this.logoPanel.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(165, 217);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // buttonEmployees
+            // 
+            this.buttonEmployees.Image = ((System.Drawing.Image)(resources.GetObject("buttonEmployees.Image")));
+            this.buttonEmployees.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonEmployees.Location = new System.Drawing.Point(23, 26);
+            this.buttonEmployees.Name = "buttonEmployees";
+            this.buttonEmployees.Size = new System.Drawing.Size(251, 74);
+            this.buttonEmployees.TabIndex = 3;
+            this.buttonEmployees.Text = "Employees";
+            this.buttonEmployees.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.Location = new System.Drawing.Point(23, 129);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(251, 74);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Time and Attendance";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // buttonLeaveRequestPortal
+            // 
+            this.buttonLeaveRequestPortal.Image = ((System.Drawing.Image)(resources.GetObject("buttonLeaveRequestPortal.Image")));
+            this.buttonLeaveRequestPortal.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonLeaveRequestPortal.Location = new System.Drawing.Point(23, 234);
+            this.buttonLeaveRequestPortal.Name = "buttonLeaveRequestPortal";
+            this.buttonLeaveRequestPortal.Size = new System.Drawing.Size(251, 74);
+            this.buttonLeaveRequestPortal.TabIndex = 5;
+            this.buttonLeaveRequestPortal.Text = "Leave Request Portal";
+            this.buttonLeaveRequestPortal.UseVisualStyleBackColor = true;
+            // 
+            // buttonLeaveManagement
+            // 
+            this.buttonLeaveManagement.Image = ((System.Drawing.Image)(resources.GetObject("buttonLeaveManagement.Image")));
+            this.buttonLeaveManagement.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonLeaveManagement.Location = new System.Drawing.Point(22, 320);
+            this.buttonLeaveManagement.Name = "buttonLeaveManagement";
+            this.buttonLeaveManagement.Size = new System.Drawing.Size(251, 74);
+            this.buttonLeaveManagement.TabIndex = 6;
+            this.buttonLeaveManagement.Text = "Leave Management";
+            this.buttonLeaveManagement.UseVisualStyleBackColor = true;
+            // 
+            // buttonPayroll
+            // 
+            this.buttonPayroll.Image = ((System.Drawing.Image)(resources.GetObject("buttonPayroll.Image")));
+            this.buttonPayroll.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonPayroll.Location = new System.Drawing.Point(23, 424);
+            this.buttonPayroll.Name = "buttonPayroll";
+            this.buttonPayroll.Size = new System.Drawing.Size(251, 74);
+            this.buttonPayroll.TabIndex = 7;
+            this.buttonPayroll.Text = "Payroll";
+            this.buttonPayroll.UseVisualStyleBackColor = true;
+            // 
+            // buttonCustomers
+            // 
+            this.buttonCustomers.Image = ((System.Drawing.Image)(resources.GetObject("buttonCustomers.Image")));
+            this.buttonCustomers.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonCustomers.Location = new System.Drawing.Point(23, 526);
+            this.buttonCustomers.Name = "buttonCustomers";
+            this.buttonCustomers.Size = new System.Drawing.Size(251, 74);
+            this.buttonCustomers.TabIndex = 8;
+            this.buttonCustomers.Text = "Customers";
+            this.buttonCustomers.UseVisualStyleBackColor = true;
+            // 
+            // buttonProjectsAndTasks
+            // 
+            this.buttonProjectsAndTasks.Location = new System.Drawing.Point(23, 627);
+            this.buttonProjectsAndTasks.Name = "buttonProjectsAndTasks";
+            this.buttonProjectsAndTasks.Size = new System.Drawing.Size(251, 74);
+            this.buttonProjectsAndTasks.TabIndex = 4;
+            this.buttonProjectsAndTasks.Text = "Projects and Tasks";
+            this.buttonProjectsAndTasks.UseVisualStyleBackColor = true;
+            // 
+            // buttonStocks
+            // 
+            this.buttonStocks.Location = new System.Drawing.Point(22, 718);
+            this.buttonStocks.Name = "buttonStocks";
+            this.buttonStocks.Size = new System.Drawing.Size(251, 74);
+            this.buttonStocks.TabIndex = 9;
+            this.buttonStocks.Text = "Stocks";
+            this.buttonStocks.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1049, 715);
+            this.ClientSize = new System.Drawing.Size(1235, 815);
+            this.Controls.Add(this.logoPanel);
             this.Controls.Add(this.topPanel);
             this.Controls.Add(this.sidePanel);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.sidePanel.ResumeLayout(false);
+            this.logoPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -67,6 +196,16 @@
 
         private System.Windows.Forms.Panel sidePanel;
         private System.Windows.Forms.Panel topPanel;
+        private System.Windows.Forms.Button buttonStocks;
+        private System.Windows.Forms.Button buttonProjectsAndTasks;
+        private System.Windows.Forms.Button buttonCustomers;
+        private System.Windows.Forms.Button buttonPayroll;
+        private System.Windows.Forms.Button buttonLeaveManagement;
+        private System.Windows.Forms.Button buttonLeaveRequestPortal;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button buttonEmployees;
+        private System.Windows.Forms.Panel logoPanel;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
