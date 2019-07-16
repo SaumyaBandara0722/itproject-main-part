25b3acf99367ec402d6a5641512aab992d3ae302
Amith Mihiranga <lwpamihiranga@gmail.com>
2019-07-16 16:37:33 +0530

    first commit

--- itproject/Form1.Designer.cs
+++ itproject/Form1.Designer.cs
@@ -28,13 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.sidePanel = new System.Windows.Forms.Panel();
+            this.topPanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // sidePanel
+            // 
+            this.sidePanel.BackColor = System.Drawing.Color.DimGray;
+            this.sidePanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.sidePanel.Location = new System.Drawing.Point(0, 0);
+            this.sidePanel.Name = "sidePanel";
+            this.sidePanel.Size = new System.Drawing.Size(294, 715);
+            this.sidePanel.TabIndex = 0;
+            // 
+            // topPanel
+            // 
+            this.topPanel.BackColor = System.Drawing.Color.DodgerBlue;
+            this.topPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.topPanel.Location = new System.Drawing.Point(294, 0);
+            this.topPanel.Name = "topPanel";
+            this.topPanel.Size = new System.Drawing.Size(755, 10);
+            this.topPanel.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1049, 715);
+            this.Controls.Add(this.topPanel);
+            this.Controls.Add(this.sidePanel);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel sidePanel;
+        private System.Windows.Forms.Panel topPanel;
     }
 }
 
