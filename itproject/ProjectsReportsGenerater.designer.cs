1e887bcea42cbb1d6a75131c03eaa4532eb67e0a
Amith Mihiranga <lwpamihiranga@gmail.com>
2019-09-30 15:04:09 +0530

    Project and Tasks report

--- itproject/ProjectsReportsGenerater.designer.cs
+++ itproject/ProjectsReportsGenerater.designer.cs
@@ -29,24 +29,23 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ProjectsReportsGenerater));
             this.lblSearch = new System.Windows.Forms.Label();
             this.txtBoxSearchByAddedDate = new System.Windows.Forms.TextBox();
-            this.dataGridViewForReports = new System.Windows.Forms.DataGridView();
-            this.DOWNLOAD = new System.Windows.Forms.DataGridViewButtonColumn();
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
-            this.projectIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.orderIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.addedDateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.deadlineDateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.descriptionDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.statusDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.projectReportsBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewForReports)).BeginInit();
-            this.tableLayoutPanel1.SuspendLayout();
-            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.Projects_TasksBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.itproject_newDataSet1 = new itproject.itproject_newDataSet1();
+            this.Projects_TasksTableAdapter = new itproject.itproject_newDataSet1TableAdapters.Projects_TasksTableAdapter();
+            this.buttonGenerate = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.projectReportsBindingSource)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Projects_TasksBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.itproject_newDataSet1)).BeginInit();
             this.SuspendLayout();
             // 
             // lblSearch
@@ -66,40 +65,10 @@
             this.txtBoxSearchByAddedDate.Name = "txtBoxSearchByAddedDate";
             this.txtBoxSearchByAddedDate.Size = new System.Drawing.Size(641, 28);
             this.txtBoxSearchByAddedDate.TabIndex = 2;
-            this.txtBoxSearchByAddedDate.TextChanged += new System.EventHandler(this.TxtBoxSearchByAddedDate_TextChanged);
             // 
-            // dataGridViewForReports
+            // projectReportsBindingSource
             // 
-            this.dataGridViewForReports.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.dataGridViewForReports.AutoGenerateColumns = false;
-            this.dataGridViewForReports.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.dataGridViewForReports.BackgroundColor = System.Drawing.Color.LightSeaGreen;
-            this.dataGridViewForReports.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridViewForReports.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.projectIDDataGridViewTextBoxColumn,
-            this.orderIDDataGridViewTextBoxColumn,
-            this.addedDateDataGridViewTextBoxColumn,
-            this.deadlineDateDataGridViewTextBoxColumn,
-            this.descriptionDataGridViewTextBoxColumn,
-            this.statusDataGridViewTextBoxColumn,
-            this.DOWNLOAD});
-            this.dataGridViewForReports.DataSource = this.projectReportsBindingSource;
-            this.dataGridViewForReports.Location = new System.Drawing.Point(3, 3);
-            this.dataGridViewForReports.Name = "dataGridViewForReports";
-            this.dataGridViewForReports.RowHeadersWidth = 51;
-            this.dataGridViewForReports.RowTemplate.Height = 24;
-            this.dataGridViewForReports.Size = new System.Drawing.Size(931, 425);
-            this.dataGridViewForReports.TabIndex = 3;
-            this.dataGridViewForReports.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellContentClick);
-            // 
-            // DOWNLOAD
-            // 
-            this.DOWNLOAD.DataPropertyName = "Status";
-            this.DOWNLOAD.HeaderText = "Reports";
-            this.DOWNLOAD.MinimumWidth = 6;
-            this.DOWNLOAD.Name = "DOWNLOAD";
+            this.projectReportsBindingSource.DataSource = typeof(itproject.ReportRelatedClasses.ProjectReports);
             // 
             // tableLayoutPanel1
             // 
@@ -118,67 +87,54 @@
             this.tableLayoutPanel1.Size = new System.Drawing.Size(937, 42);
             this.tableLayoutPanel1.TabIndex = 4;
             // 
-            // tableLayoutPanel2
+            // dateTimePicker1
             // 
-            this.tableLayoutPanel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.tableLayoutPanel2.BackColor = System.Drawing.Color.Transparent;
-            this.tableLayoutPanel2.ColumnCount = 1;
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel2.Controls.Add(this.dataGridViewForReports, 0, 0);
-            this.tableLayoutPanel2.Location = new System.Drawing.Point(88, 152);
-            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
-            this.tableLayoutPanel2.RowCount = 1;
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(937, 431);
-            this.tableLayoutPanel2.TabIndex = 5;
+            this.dateTimePicker1.Location = new System.Drawing.Point(381, 33);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(272, 22);
+            this.dateTimePicker1.TabIndex = 5;
             // 
-            // projectIDDataGridViewTextBoxColumn
+            // dateTimePicker2
             // 
-            this.projectIDDataGridViewTextBoxColumn.DataPropertyName = "ProjectID";
-            this.projectIDDataGridViewTextBoxColumn.HeaderText = "ProjectID";
-            this.projectIDDataGridViewTextBoxColumn.MinimumWidth = 6;
-            this.projectIDDataGridViewTextBoxColumn.Name = "projectIDDataGridViewTextBoxColumn";
+            this.dateTimePicker2.Location = new System.Drawing.Point(659, 33);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(264, 22);
+            this.dateTimePicker2.TabIndex = 6;
             // 
-            // orderIDDataGridViewTextBoxColumn
+            // reportViewer1
             // 
-            this.orderIDDataGridViewTextBoxColumn.DataPropertyName = "OrderID";
-            this.orderIDDataGridViewTextBoxColumn.HeaderText = "OrderID";
-            this.orderIDDataGridViewTextBoxColumn.MinimumWidth = 6;
-            this.orderIDDataGridViewTextBoxColumn.Name = "orderIDDataGridViewTextBoxColumn";
+            reportDataSource1.Name = "ProjectTaskDataSet";
+            reportDataSource1.Value = this.Projects_TasksBindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "itproject.Report3.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(82, 175);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.Size = new System.Drawing.Size(940, 463);
+            this.reportViewer1.TabIndex = 7;
             // 
-            // addedDateDataGridViewTextBoxColumn
+            // Projects_TasksBindingSource
             // 
-            this.addedDateDataGridViewTextBoxColumn.DataPropertyName = "AddedDate";
-            this.addedDateDataGridViewTextBoxColumn.HeaderText = "AddedDate";
-            this.addedDateDataGridViewTextBoxColumn.MinimumWidth = 6;
-            this.addedDateDataGridViewTextBoxColumn.Name = "addedDateDataGridViewTextBoxColumn";
+            this.Projects_TasksBindingSource.DataMember = "Projects_Tasks";
+            this.Projects_TasksBindingSource.DataSource = this.itproject_newDataSet1;
             // 
-            // deadlineDateDataGridViewTextBoxColumn
+            // itproject_newDataSet1
             // 
-            this.deadlineDateDataGridViewTextBoxColumn.DataPropertyName = "DeadlineDate";
-            this.deadlineDateDataGridViewTextBoxColumn.HeaderText = "DeadlineDate";
-            this.deadlineDateDataGridViewTextBoxColumn.MinimumWidth = 6;
-            this.deadlineDateDataGridViewTextBoxColumn.Name = "deadlineDateDataGridViewTextBoxColumn";
+            this.itproject_newDataSet1.DataSetName = "itproject_newDataSet1";
+            this.itproject_newDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
             // 
-            // descriptionDataGridViewTextBoxColumn
+            // Projects_TasksTableAdapter
             // 
-            this.descriptionDataGridViewTextBoxColumn.DataPropertyName = "Description";
-            this.descriptionDataGridViewTextBoxColumn.HeaderText = "Description";
-            this.descriptionDataGridViewTextBoxColumn.MinimumWidth = 6;
-            this.descriptionDataGridViewTextBoxColumn.Name = "descriptionDataGridViewTextBoxColumn";
+            this.Projects_TasksTableAdapter.ClearBeforeFill = true;
             // 
-            // statusDataGridViewTextBoxColumn
+            // buttonGenerate
             // 
-            this.statusDataGridViewTextBoxColumn.DataPropertyName = "Status";
-            this.statusDataGridViewTextBoxColumn.HeaderText = "Status";
-            this.statusDataGridViewTextBoxColumn.MinimumWidth = 6;
-            this.statusDataGridViewTextBoxColumn.Name = "statusDataGridViewTextBoxColumn";
-            // 
-            // projectReportsBindingSource
-            // 
-            this.projectReportsBindingSource.DataSource = typeof(itproject.ReportRelatedClasses.ProjectReports);
+            this.buttonGenerate.Location = new System.Drawing.Point(564, 120);
+            this.buttonGenerate.Name = "buttonGenerate";
+            this.buttonGenerate.Size = new System.Drawing.Size(192, 30);
+            this.buttonGenerate.TabIndex = 8;
+            this.buttonGenerate.Text = "Generate";
+            this.buttonGenerate.UseVisualStyleBackColor = true;
+            this.buttonGenerate.Click += new System.EventHandler(this.buttonGenerate_Click);
             // 
             // ProjectsReportsGenerater
             // 
@@ -187,16 +143,19 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1108, 687);
-            this.Controls.Add(this.tableLayoutPanel2);
+            this.Controls.Add(this.buttonGenerate);
+            this.Controls.Add(this.reportViewer1);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "ProjectsReportsGenerater";
             this.Text = "ProjectsReportsGenerater";
             this.Load += new System.EventHandler(this.ProjectsReportsGenerater_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewForReports)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.projectReportsBindingSource)).EndInit();
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
-            this.tableLayoutPanel2.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.projectReportsBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Projects_TasksBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.itproject_newDataSet1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -204,16 +163,14 @@
         #endregion
         private System.Windows.Forms.Label lblSearch;
         private System.Windows.Forms.TextBox txtBoxSearchByAddedDate;
-        private System.Windows.Forms.DataGridView dataGridViewForReports;
-        private System.Windows.Forms.DataGridViewTextBoxColumn projectIDDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn orderIDDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn addedDateDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn deadlineDateDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn descriptionDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn statusDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewButtonColumn DOWNLOAD;
         private System.Windows.Forms.BindingSource projectReportsBindingSource;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private System.Windows.Forms.BindingSource Projects_TasksBindingSource;
+        private itproject_newDataSet1 itproject_newDataSet1;
+        private itproject_newDataSet1TableAdapters.Projects_TasksTableAdapter Projects_TasksTableAdapter;
+        private System.Windows.Forms.Button buttonGenerate;
     }
 }
---
+++ itproject/itproject_newDataSet1.cs
@@ -0,0 +1,15 @@
+﻿namespace itproject
+{
+
+
+    partial class itproject_newDataSet1
+    {
+    }
+}
+
+namespace itproject.itproject_newDataSet1TableAdapters {
+    
+    
+    public partial class Projects_TasksTableAdapter {
+    }
+}
