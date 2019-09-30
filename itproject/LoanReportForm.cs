2bdd340fe9af31cf5c0377e078d771e8c8e741ce
Amith Mihiranga <lwpamihiranga@gmail.com>
2019-09-30 12:03:08 +0530

    Loan report completed

---
+++ itproject/LoanReportForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace itproject
+{
+    public partial class LoanReportForm : Form
+    {
+        public LoanReportForm()
+        {
+            InitializeComponent();
+        }
+
+        private void LoanReportForm_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'itproject_newDataSet.Loans' table. You can move, or remove it, as needed.
+            // this.LoansTableAdapter.Fill(this.itproject_newDataSet.Loans);
+
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'itproject_newDataSet.Loans' table. You can move, or remove it, as needed.
+            this.LoansTableAdapter.Fill(this.itproject_newDataSet.Loans, dateTimePicker1.Value.ToString(), dateTimePicker2.Value.ToString());
+
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
--- itproject/Properties/Settings.Designer.cs
+++ itproject/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace itproject.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.2.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.3.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -30,6 +30,16 @@ namespace itproject.Properties {
         public string itprojectConnectionString {
             get {
                 return ((string)(this["itprojectConnectionString"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=AMITH-PC;Initial Catalog=itproject_new;Integrated Security=True")]
+        public string itproject_newConnectionString {
+            get {
+                return ((string)(this["itproject_newConnectionString"]));
             }
         }
     }
